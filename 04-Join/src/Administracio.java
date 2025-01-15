f69ec1e1761409fce92f5b79367b0a8405d94ec8
Pau Pedrós <2023_pau.pedros@iticbcn.cat>
2025-01-15 20:20:05 +0100

    04-Join: Modularitzat metodes del main

--- 04-Join/src/Administracio.java
+++ 04-Join/src/Administracio.java
@@ -10,26 +10,36 @@ public class Administracio {
 
   public static void main(String[] args) {
 
+    // Inicialitzem l'administració per crear als treballadors
     Administracio adm = new Administracio();
 
     // Posem en marxa
+    marxa();
+
+    // Esperem als altres fils a acabar
+    espera();
+
     for (Treballador treb : poblacio_activa) {
-      try {
-        treb.start();
-      } catch (Exception e) {
-      }
+      System.out.printf("%-12s -> edat: %d / total: %.2f\n", treb.getName(), treb.getEdat(), treb.getCobrat());
     }
 
+  }
+
+  private static void espera() {
     for (Treballador treb : poblacio_activa) {
       try {
         treb.join();
       } catch (Exception e) {
       }
     }
+  }
 
+  private static void marxa() {
     for (Treballador treb : poblacio_activa) {
-      System.out.printf("%-12s -> edat: %d / total: %.2f\n", treb.getName(), treb.getEdat(), treb.getCobrat());
+      try {
+        treb.start();
+      } catch (Exception e) {
+      }
     }
-
   }
 }
